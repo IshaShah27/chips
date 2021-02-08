ed4e1488cd8fe52fec4c4fd22577951a9fe20dd4
is2404 Shah <is2404@columbia.edu>
2021-02-08 12:03:12 +0000

    12 work but not 3

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,20 +5,30 @@ class MoviesController < ApplicationController
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
-
+  
   def index
     @movies = Movie.all
-    @all_ratings = %w[G PG PG-13 R]
-    #@all_ratings = Movie.all_ratings
+    
+    # filtering
+    @all_ratings = Movie.all_ratings
+    
     if params[:ratings]==nil
       @ratings_to_show = []
       @movies = Movie.all
+      @selected_ratings = @all_ratings
     else
-      @ratings_to_show = params[:ratings].keys 
+      @ratings_to_show = params[:ratings].keys
       @movies = Movie.with_ratings(@ratings_to_show)
+      @selected_ratings = @ratings_to_show
     end
+    
+    # sorting
     if params[:sort] != nil
+      @selected_sort = params[:sort]
       @movies = @movies.order(params[:sort])
+      #.with_ratings(@ratings_to_show)
+     # redirect_to movies_path({order_by: @selected_sort, ratings: @selected_ratings})
+
       if params[:sort] == "title"
         @title_highlight = "hilite"
       end
